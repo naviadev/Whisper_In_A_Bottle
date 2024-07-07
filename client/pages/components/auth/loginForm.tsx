ff9ad90890eb83a0703b92515f415dcf753648f7
naviadev <qo2oq1020@gmail.com>
2024-07-07 18:05:27 +0900

    WIB-14 Feat | loginForm.tsx : login section 컴포넌트 추가

---
+++ client/pages/components/auth/loginForm.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const loginForm = () => {
+  return (
+    <div className="login-form">
+      <input type="email" placeholder="test" />
+      <input type="password" placeholder="test" />
+      <button type="submit">send</button>
+    </div>
+  );
+};
+
+export default loginForm;
