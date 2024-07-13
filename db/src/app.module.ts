eecb57b27af30cdf4962c0828c4bf403f718b7fc
moonhr <moonhr153@gmail.com>
2024-07-13 13:10:52 +0900

    WIB-6 Fix | app모듈에 사용자모듈과 config모듈 추가

--- db/src/app.module.ts
+++ db/src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '../config/config.module';
+import { AuthModule } from '../auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,7 +8,12 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), PlayerModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    PlayerModule,
+    ConfigModule, // ConfigModule을 import하여 환경 변수 로드
+    AuthModule, // AuthModule을 import하여 JWT 인증 설정
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
