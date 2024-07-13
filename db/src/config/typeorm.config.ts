41c6b8ff0559e3be6ca4cde05fbdb06c5b8971a8
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-13 11:51:45 +0900

    WIB-9 New | typeorm.config.js : PostgreSQL 데이터베이스에 연결하고 TypeORM을 설정하는 데 사용하는 config

---
+++ db/src/config/typeorm.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: process.env.TYPEORM_SYNC === 'true',
+};
