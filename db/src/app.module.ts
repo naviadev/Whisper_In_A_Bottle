3120a74ea7db0968ac5f422d7ba9b7f92feb329a
moonhr <moonhr153@gmail.com>
2024-07-13 15:58:02 +0900

    WIB-6 Rename | jest 사용을 위한 파일 경로 이동
    jest 설정시 src내 파일을 검토함으로
    생성된 파일을 src내부로 이동함

--- db/src/app.module.ts
+++ db/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '../config/config.module';
-import { AuthModule } from '../auth/auth.module';
+import { ConfigModule } from './config/config.module';
+import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
