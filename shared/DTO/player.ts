37fd702cf748f8a2491fe84325f58258672d0101
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-14 21:45:56 +0900

    WIB-9 Fix | player.ts : export 내보내기 구문 추가

--- shared/DTO/player.ts
+++ shared/DTO/player.ts
@@ -1,7 +1,7 @@
 import { User } from "./userInterface"
 import { Letter } from "./letter"
 
-interface Player extends User {
+export interface Player extends User {
   receiveTime: number,
   sendTime: number,
   isAssign: boolean,
