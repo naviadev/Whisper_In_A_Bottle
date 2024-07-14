8fedf3cc68ebe094407648ebcd363bdfc0727771
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-14 22:47:29 +0900

    WIB-9 New | player.enitity.ts : player 테이블에대한 entitiy 작성

---
+++ db/src/player/player.entity.ts
@@ -0,0 +1,16 @@
+import { BaseEntity, Entity, PrimaryColumn, Column } from 'typeorm'; // TypeORM 데코레이터와 클래스들을 가져옵니다.
+
+/**
+ * * 데이터베이스 테이블 plyer (name) 에 매핑한다.
+ */
+@Entity({ name: 'player' })
+export class Player extends BaseEntity {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  sendTime: number;
+}
