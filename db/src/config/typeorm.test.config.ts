b38bbbbe4cdd4d81cbcb2cf823de29904c17e998
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-16 11:25:26 +0900

    WIB-9 Test | typeorm.test.config.ts : Jest에서 사용될 typeorm config 파일

---
+++ db/src/config/typeorm.test.config.ts
@@ -0,0 +1,28 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+import { join } from 'path';
+import * as fs from 'fs';
+
+dotenv.config();
+
+const sslOptions =
+  process.env.DB_SSL === 'true'
+    ? {
+        rejectUnauthorized: false,
+        ca: fs
+          .readFileSync(join(__dirname, 'ap-northeast-2-bundle.pem'))
+          .toString(),
+      }
+    : false;
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_TEST_NAME,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: process.env.TYPEORM_SYNC === 'true',
+  ssl: sslOptions,
+};
