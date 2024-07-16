de13f4087306b3b88e8f2240c13886fec780be3c
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-16 15:36:36 +0900

    WIB-9 Feat | app.module.ts : TypeOrmMoudle, PlayerModule 연결

--- db/src/app.module.ts
+++ db/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
+import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), PlayerModule],
   controllers: [AppController],
   providers: [AppService],
 })
