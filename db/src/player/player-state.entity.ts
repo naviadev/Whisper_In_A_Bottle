b3a6792b98596b9835c47dd1d918956569001263
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-17 23:41:17 +0900

    WIB-24 New | player-state.entity.ts : 플레이어 상세 정보에 대한 엔티티 생성

---
+++ db/src/player/player-state.entity.ts
@@ -0,0 +1,19 @@
+import { BaseEntity, Entity, PrimaryColumn, Column } from 'typeorm'; // TypeORM 데코레이터와 클래스들을 가져옵니다.
+
+/**
+ * * 데이터베이스 테이블 plyer (name) 에 매핑한다.
+ */
+@Entity({ name: 'player_state' })
+export class PlayerState extends BaseEntity {
+  @PrimaryColumn()
+  user_id: string;
+
+  @Column('bigint')
+  receive_time: number;
+
+  @Column('bigint')
+  last_login_time: number;
+
+  @Column()
+  last_login_ip: string;
+}
