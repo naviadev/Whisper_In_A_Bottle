b021d1d65680f0c66e8398767f466d7f816f2ef2
naviadev <qo2oq1020@gmail.com>
2024-07-21 21:35:23 +0900

    WIB-14 Fix | main.ts - server : enableCors 3000 Port

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
