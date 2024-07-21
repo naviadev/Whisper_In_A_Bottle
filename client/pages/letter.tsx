6bb36256254a02d7825be4531c11e271704b0ba1
moonhr <moonhr153@gmail.com>
2024-07-21 20:51:39 +0900

    WIB-35 Feat | letter.tsx : 편지 송수신 창

---
+++ client/pages/letter.tsx
@@ -0,0 +1,20 @@
+// 편지 보여질 창
+import React, { useState } from "react";
+
+import LetterComposer from "./components/letter/LetterComposer";
+import ReceivedLettersButton from "./components/letter/ReceivedLettersButton";
+import ReceivedLetterViewer from "./components/letter/ReceivedLetterViewer";
+
+const LettersPage: React.FC = () => {
+  const [letterContent, setLetterContent] = useState<string | null>(null);
+
+  return (
+    <div>
+      <LetterComposer />
+      <ReceivedLettersButton />
+      <ReceivedLetterViewer letterContent={letterContent} />
+    </div>
+  );
+};
+
+export default LettersPage;
