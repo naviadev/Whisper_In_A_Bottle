a1df3eafcf505263fbdfd1cf8d3d17b92e040e6a
moonhr <moonhr153@gmail.com>
2024-07-21 19:26:02 +0900

    WIB-31 Fix | main.ts CORS설정
    기본 cors설정과 웹소켓cors설정

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomIoAdapter } from './custom-io-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // HTTP 서버의 CORS 설정
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  // WebSocket CORS 설정
+  app.useWebSocketAdapter(new CustomIoAdapter(app));
+
+  await app.listen(3001);
 }
 bootstrap();
