c839f9d935cc8204421f080cc44383f52142572f
moonhr <moonhr153@gmail.com>
2024-07-24 15:22:32 +0900

    WIB-6 Fix | auth.controller.ts : 사용자 로직 컨트롤러

--- auth-service/src/modules/controllers/auth.controller.ts
+++ auth-service/src/modules/controllers/auth.controller.ts
@@ -1,33 +1,46 @@
-import {
-  Controller,
-  Post,
-  Body,
-  UnauthorizedException,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { PlayerDTO } from '@shared/DTO/sharedDTO';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { Request } from '@nestjs/common';
 
-@Controller('auth') //auth 경로로 들어오는 모든 요청을 처리
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('login') //login 경로로 들어오는 post 요청을 처리
-  async login(@Body() user: PlayerDTO) {
-    const { id, password } = user;
-    const validatedUser = await this.authService.validateUser(id, password);
-    if (!validatedUser) {
-      throw new UnauthorizedException('Invalid credentials');
+  /**
+   * * 로그인 엔드포인트
+   * 작성자 : @moonhr / 2024-07-23
+   * 편집자 : @moonhr / 2024-07-24
+   * Issue : WIB-6
+   * @decorator Post
+   * @description
+   */
+  @Post('login')
+  async login(@Body() data: PlayerDTO): Promise<{ access_token: string }> {
+    const isValid = await this.authService.validateDTO(data);
+    if (!isValid) {
+      throw new Error('유효하지 않음.');
     }
-    return this.authService.login(validatedUser);
+
+    // 사용자 검증 및 로그인 처리
+    const user = await this.authService.validateUser(data.id, data.password);
+    if (!user) {
+      throw new Error('Invalid credentials');
+    }
+
+    return this.authService.login(data);
   }
 
-  @UseGuards(JwtAuthGuard) // 로그아웃도 인증이 필요한 경우
-  @Post('logout') //logout 경로로 들어오는 post 요청을 처리
-  async logout() {
-    return this.authService.logout();
+  /**
+   * 토큰 검증 엔드포인트 JwtAuthGuard를 거쳐 검증
+   * @param request Request 객체
+   * @returns 토큰이 유효하면 true, 그렇지 않으면 false
+   */
+  @Post('validate-token')
+  @UseGuards(JwtAuthGuard)
+  async validateToken(@Request() request): Promise<boolean> {
+    const token = request.headers.authorization.split(' ')[1];
+    return this.authService.validateToken(token);
   }
-
-  //회원가입 경로 처리 추가예정
 }
