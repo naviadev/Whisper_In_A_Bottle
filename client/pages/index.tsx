afb57da27b82bd2f47e172aa45fab63f0885dbbd
moonhr <moonhr153@gmail.com>
2024-07-27 13:17:47 +0900

    WIB-6 Fix | test를 위한 주석

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,13 @@
-import Socket from "./components/socket";
-
 import React from "react";
 
+// import Socket from "./components/socket";
 import RegisterForm from "./components/auth/registerForm";
 
 const HomePage = () => {
   return (
     <div>
       <h1>Welcome to the Letter Sending App</h1>
-      <Socket />
+      {/* <Socket /> */}
       <RegisterForm />
     </div>
   );
