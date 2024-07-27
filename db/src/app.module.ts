8251bf1c89acc3b2d91ad6a72f041d783fbaafb9
moonhr <moonhr153@gmail.com>
2024-07-27 13:23:45 +0900

    WIB-6 Style

--- db/src/app.module.ts
+++ db/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from './config/config.module';
-import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -8,12 +6,7 @@ import { typeOrmConfig } from './config/typeorm.config';
 import { PlayerModule } from './player/player.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
-    PlayerModule,
-    ConfigModule, // ConfigModule을 import하여 환경 변수 로드
-    AuthModule, // AuthModule을 import하여 JWT 인증 설정
-  ],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), PlayerModule],
   controllers: [AppController],
   providers: [AppService],
 })
