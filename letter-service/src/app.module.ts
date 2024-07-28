f3ffbd0debebe9d32cc8a46895a5a963f8d61593
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-28 17:23:16 +0900

    WIB-38 Fix | letter-service/app.module.ts : app.service. app.controller삭제로인한 변경

--- letter-service/src/app.module.ts
+++ letter-service/src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LetterModule } from './letter/letter.module';
 
 @Module({
   imports: [LetterModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
