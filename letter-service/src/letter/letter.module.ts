eaf630309c28922ffbfdcc0736b137b56474b75f
jaemin1005 <ghkdwoals1005@gmail.com>
2024-07-31 19:52:03 +0900

    WIB-38 Feat | letter-service/letter.module.ts : TypoeOrmModule, Controller, Service 추가

--- letter-service/src/letter/letter.module.ts
+++ letter-service/src/letter/letter.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-// import { LetterController } from './letter.controller';
-// import { LetterService } from './letter.service';
+import { LetterController } from './letter.controller';
+import { LetterService } from './letter.service';
 import { LetterGateway } from './letter.gateway';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LetterState } from './entities/letter-state.entity';
+import { UserState } from './entities/user-state.entity';
+import { Letter } from './entities/letter.entity';
+import { typeOrmConfig } from './config/env.config';
+import { LetterDbService } from './letter-db.service';
 @Module({
-  providers: [LetterGateway],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TypeOrmModule.forFeature([LetterState, UserState, Letter]),
+  ],
+  controllers: [LetterController],
+  providers: [LetterGateway, LetterService, LetterDbService],
 })
 export class LetterModule {}
