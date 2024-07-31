5600b2dcf0579038e94ce8aeb7739494869bc692
naviadev <qo2oq1020@gmail.com>
2024-07-31 19:45:26 +0900

    WIB-27 Fix | useLoginHooks.ts : 로그인 성공 -> route 를 통해 URL 변경

--- client/hooks/authView/useLoginHooks.ts
+++ client/hooks/authView/useLoginHooks.ts
@@ -1,18 +1,23 @@
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 import ValidateId from "../../models/validators/ValidateId";
 import LoginAxios from "../../models/services/loginAxios";
 
 /**
- * * Login View와 Model(login, ValidateEmail) 상호 작용하는 ViewModel
- * * SPR원칙, UI 상태를 관리하고, 사용자 입력을 처리하며, Model과 상호작용하는 책임
- * * UI와 독립적으로 테스트 할 수 있다는 장점이 있음
- * @returns React Hook
+ * * Function : useLoginHooks
+ * 작성자 : @jaemin1005 / 2024-07-?
+ * 편집자 : @naviadev / 2024-07-28
+ * Issue : WIB-27
+ * @function useLoginHooks
+ * @description 
+
  */
 const useLoginHooks = () => {
   const [id, setId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const router = useRouter();
 
   const handleLogin = async (): Promise<boolean> => {
     //* 공백이 있으면 안됨
@@ -28,8 +33,10 @@ const useLoginHooks = () => {
     const success = await LoginAxios({ playerID: id, password: password });
     if (success) {
       setIsLoggedIn(true);
+      router.push("/letterView");
       return true;
     } else {
+      console.error("Login failed");
       return false;
     }
   };
