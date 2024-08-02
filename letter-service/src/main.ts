0b629d8fb909962a1bc41e95ce388587f8bcfff7
moonhr <moonhr153@gmail.com>
2024-08-02 20:32:22 +0900

    WIB-28 Fix | main.ts websocket cors 설정

--- letter-service/src/main.ts
+++ letter-service/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CustomIoAdapter } from './letter/custom-io-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,8 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+  //WebSocket CORS 설정
+  app.useWebSocketAdapter(new CustomIoAdapter(app));
 
   await app.listen(3002);
 }
