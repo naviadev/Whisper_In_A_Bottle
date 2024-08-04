cdf2cd5a82ac34380caddba01ac056a65c0a0170
naviadev <qo2oq1020@gmail.com>
2024-08-04 17:40:00 +0900

    Feat | LetterDTO : Class 형태의 DTO 추가, ApiProperty 추가.

--- letter-service/src/letter/dto/letter.dto.ts
+++ letter-service/src/letter/dto/letter.dto.ts
@@ -1,6 +1,26 @@
+import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export interface LetterDto {
   body: string;
   time: number;
   id: string;
+  ip: string;
+}
+
+export class LetterDTO {
+  @ApiProperty({ description: '편지 본문' })
+  @IsString()
+  body: string;
+
+  @ApiProperty({ description: '편지를 보낸 시간' })
+  @IsNumber()
+  time: number;
+
+  @ApiProperty({ description: '송신자의 ID' })
+  @IsString()
+  id: string;
+
+  @ApiProperty({ description: '송신자의 IP' })
+  @IsString()
   ip: string;
 }
