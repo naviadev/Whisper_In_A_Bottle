4b6ba0de6a7025a066e572e1546b18fe036149ae
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-04 17:07:21 +0900

    WIB-38 Fix | letter-serivce.ts : letterDto 수정, 사용안하는 키 제거

--- letter-service/src/letter/dto/letter.dto.ts
+++ letter-service/src/letter/dto/letter.dto.ts
@@ -1,6 +1,7 @@
-export interface LetterDto {
-  body: string;
-  time: number;
+export interface SenderLetterDto {
+  content: string;
+}
+
+export interface ReceiveLetterDto extends SenderLetterDto {
   id: string;
-  ip: string;
 }
