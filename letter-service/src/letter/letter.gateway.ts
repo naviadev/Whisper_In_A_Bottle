226950280942fa7724643b69699eb21a2360e83b
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-04 17:16:08 +0900

    WIB-38 Feat | letter-service/letter.gateway.ts : 양방향 통신을 위해 로직을 변경하였습니다.

--- letter-service/src/letter/letter.gateway.ts
+++ letter-service/src/letter/letter.gateway.ts
@@ -7,12 +7,23 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
-import { LetterDto } from './dto/letter.dto';
+import { ReceiveLetterDto, SenderLetterDto } from './dto/letter.dto';
+import { LetterLogicService } from './letter-logic.service';
+import { LetterState } from './entities/letter-state.entity';
+
+export enum LETTER_MSG {
+  INIT_MSG = 'initial_data',
+  INIT_MSG_RECEVIED = 'initial_data.received',
+  RECEIVE_MSG = 'espresso',
+  RECEIVE_MSG_RECEVIED = 'espresspo_received',
+  SEND_MSG = 'latte',
+}
 
 @WebSocketGateway()
 export class LetterGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  constructor(private readonly letterLogicService: LetterLogicService) {}
   @WebSocketServer() server: Server;
 
   //! clientid는 고유한 값을 생성하기 때문에, 중복되는 일이 없다.
@@ -55,9 +66,9 @@ export class LetterGateway
   }
 
   //* 처음에 접속했을 떄, 사용자 정보 저장
-  @SubscribeMessage('initial_data')
-  handleInitialData(client: Socket, userId: string): void {
-    if (!this.userIdKeyMap.has(userId)) {
+  @SubscribeMessage(LETTER_MSG.INIT_MSG)
+  async handleInitialData(client: Socket, userId: string) {
+    if (this.userIdKeyMap.has(userId) === false) {
       this.userIdKeyMap.set(userId, new Set());
     }
 
@@ -65,11 +76,58 @@ export class LetterGateway
     clientIds.add(client.id);
     this.clientIdKeyMap.set(client.id, userId);
 
-    client.emit('data_received', { status: 'success' });
+    client.emit('initial_data_received', 'success');
+
+    //* 받아야하는 편지 찾는 과정
+    this.letterLogicService.searchLetterState(userId).then((res) => {
+      if (res) this.readySendMassageToClient(res);
+    });
+
+    client.emit(LETTER_MSG.INIT_MSG_RECEVIED, 'success');
+  }
+
+  @SubscribeMessage(LETTER_MSG.RECEIVE_MSG)
+  async receiveLetter(client: Socket, letter: SenderLetterDto) {
+    if (this.clientIdKeyMap.has(client.id) === false) {
+      return;
+    }
+
+    const userId = this.clientIdKeyMap.get(client.id);
+
+    const letterState = await this.letterLogicService.saveAndAssignLetter(
+      userId,
+      letter,
+    );
+    if (letterState) this.readySendMassageToClient(letterState);
+    client.emit(LETTER_MSG.RECEIVE_MSG_RECEVIED, letterState);
+  }
+
+  async readySendMassageToClient(letterState: LetterState) {
+    if (this.userIdKeyMap.has(letterState.receiverId) === false) return;
+
+    const letter = await this.letterLogicService.searchLetter(
+      letterState.letterId,
+    );
+
+    const receivedLetter: ReceiveLetterDto = {
+      id: letterState.senderId,
+      content: letter.content,
+    };
+
+    //* 시간 계산
+    let calcTime = letterState.sendTime - new Date().getTime();
+    calcTime = calcTime > 0 ? calcTime : 0;
+
+    const timeoutId = setTimeout(() => {
+      this.sendMessageToClient(letterState.receiverId, receivedLetter);
+      this.userSetTimeoutKeyMap.delete(letterState.receiverId);
+    }, calcTime);
+
+    this.saveTimeoutId(letterState.receiverId, timeoutId);
   }
 
   //* 메세지 보내는 Method
-  sendMessageToClient(userId: string, letter: LetterDto) {
+  sendMessageToClient(userId: string, letter: ReceiveLetterDto) {
     const clientIds = this.userIdKeyMap.get(userId);
 
     if (!clientIds) {
@@ -82,11 +140,19 @@ export class LetterGateway
     clientIds.forEach((clientId) => {
       const client = this.server.sockets.sockets.get(clientId);
       if (client) {
-        client.emit('new_message', letter);
+        client.emit(LETTER_MSG.SEND_MSG, letter);
         isSuccess = true;
       }
     });
 
     return isSuccess;
+  }
+
+  saveTimeoutId(userId: string, timeoutId: NodeJS.Timeout) {
+    if (this.userSetTimeoutKeyMap.has(userId)) {
+      //TODO: 이미 편지가 있는데 왜??
+    } else {
+      this.userSetTimeoutKeyMap.set(userId, timeoutId);
+    }
   }
 }
