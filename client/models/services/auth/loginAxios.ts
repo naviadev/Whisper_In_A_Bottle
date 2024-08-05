17961d8bc7b2d51cbb55865b29935327a3ccbfa7
moonhr <moonhr153@gmail.com>
2024-08-05 21:59:48 +0900

    WIB-44 Fix | loginAxios.ts : fetch 구문 axios로 수정

--- client/models/services/auth/loginAxios.ts
+++ client/models/services/auth/loginAxios.ts
@@ -1,32 +1,19 @@
+import axios, { AxiosResponse } from "axios";
+
 import REQUEST_PORT from "../../../ts/enums/REQUEST_PORT.enum";
 import IPlayerDTO from "../../../ts/DTOs/IPlayerDTO";
 
-interface LoginResponse {
-  success: boolean;
-  token?: string;
-  message?: string;
-}
-
 const LoginAxios = async (user: IPlayerDTO): Promise<boolean> => {
   try {
-    const response = await fetch(REQUEST_PORT.__LOGIN_PORT, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
+    const response: AxiosResponse = await axios.post(
+      REQUEST_PORT.__LOGIN_PORT,
+      user,
+      {
+        withCredentials: true, // 요청에 쿠키를 포함하여 서버가 클라이언트를 인증
       },
-      body: JSON.stringify(user),
-      credentials: "include",
-    });
+    );
 
-    //* response.ok인 경우는 다음과 같습니다
-    //* 1. Http Status Code 200 ~ 299
-    //* 2. 네트워크 오류가 없이 옳바르게 전송되었을 떄
-    //* 둘 중 하나라도 만족하면 true입니다.
-    if (!response.ok) {
-      throw new Error("Network Error");
-    }
-    // 응답 본문을 JSON으로 파싱
-    const data: LoginResponse = await response.json();
+    const data = response.data;
 
     // 성공 여부를 반환
     return data.success;
