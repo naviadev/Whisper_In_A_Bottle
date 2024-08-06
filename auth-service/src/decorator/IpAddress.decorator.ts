5cc4ddf4178edcf9c9ae594e80048beee9e77e5d
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-06 20:21:56 +0900

    WIB-43 Rename | IpAddress.ts -> IpAddress.decorator.ts: 변경

---
+++ auth-service/src/decorator/IpAddress.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const IpAddress = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.headers['x-forwarded-for'] || request.ip;
+  },
+);
