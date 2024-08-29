ede378deba0f5cefc041a7960b3fe2c9c92b8636
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-29 20:31:28 +0900

    Fix | letter_logic.service.ts : 편지를 할당하는 로직을 변경하였습니다
    편지가 오면 letter_info를 저장 후, 이 letter_info를 이용하여 가장 오래된 편지를 찾아 할당하게 하였습니다

--- packages/letter-service/src/letter/letter_logic.service.ts
+++ packages/letter-service/src/letter/letter_logic.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { SenderLetterDTO } from '../../../../shared/dtos/letter.dto';
 import { LetterDbService } from './letter_db.service';
+import { LetterState } from '../../../../shared/entities/letter_state.entity';
 
 @Injectable()
 export class LetterLogicService {
@@ -9,15 +10,28 @@ export class LetterLogicService {
   //* 유저가 보낸 편지를 랜덤 유저를 선정 후, LetterState 형태로 저장한다.
   async saveAndAssignLetter(id: string, letter: SenderLetterDTO) {
     // TODO: 편지 저장
-    const letterEntity = await this.letterDbService.saveLetter(letter.content);
+    const letterEnitity = await this.letterDbService.saveLetter(letter.content);
+    await this.letterDbService.saveLetterInfo(id, letterEnitity);
 
     // TODO: RandomPlayer 선정
     const selectUser =
       await this.letterDbService.getUserWithLongestReceiveTime(id);
 
+    // TODO: 선정실패 X
     if (selectUser === null) {
       return;
     }
+
+    return this.SendLetter(selectUser.id);
+  }
+
+  async SendLetter(sendUserId: string): Promise<LetterState> {
+    //* 편지를 가져온다. 가장 오래된 편지
+    const letterEntity = await this.letterDbService.getOlderUnsentLetter();
+
+    await this.letterDbService.letterUpdateIsSendIntoTrue(
+      letterEntity.letter_id,
+    );
 
     // TODO: IP 비교 핑차이로
     //! 현재는 임시로 시간을 지정했다.
@@ -27,8 +41,8 @@ export class LetterLogicService {
     // TODO: LetterState에 저장
     return await this.letterDbService.saveLetterState(
       letterEntity.letter_id,
-      id,
-      selectUser.id,
+      letterEntity.user_id,
+      sendUserId,
       currentTime + addTime,
     );
   }
