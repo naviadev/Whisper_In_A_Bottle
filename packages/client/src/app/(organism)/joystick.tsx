b7e094e71ddab3eca08d14dac4abfe09735f2345
moonhr <moonhr153@gmail.com>
2024-08-29 12:30:02 +0900

    Feat | 조이스틱 부분 설계 추가부분

---
+++ packages/client/src/app/(organism)/joystick.tsx
@@ -0,0 +1,13 @@
+// src/client/joystick/joystick.tsx
+import React from "react";
+
+const Joystick: React.FC = () => {
+  return (
+    <div className="w-60 h-40 bg-zinc-200">
+      <p>Joystick Component</p>
+      {/* Joystick에 대한 추가 콘텐츠를 여기에 삽입할 수 있습니다 */}
+    </div>
+  );
+};
+
+export default Joystick;
