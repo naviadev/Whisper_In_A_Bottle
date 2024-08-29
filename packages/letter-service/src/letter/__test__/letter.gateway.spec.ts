ed7227ace4371a9f1904a2dc71dd6a7621481818
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-29 20:40:39 +0900

    Test | letter.gateway.ts : 에 대한 로직을 검증 ( 토큰은 모킹하여 테스트를 진행 )

--- packages/letter-service/src/letter/__test__/letter.gateway.spec.ts
+++ packages/letter-service/src/letter/__test__/letter.gateway.spec.ts
@@ -16,8 +16,13 @@ import {
   user_2,
   user_3,
   user_4,
+  userToken_1,
+  userToken_2,
+  userToken_3,
 } from './letter-dummyData';
 import { ReceiveLetterDTO } from '../../../../../shared/dtos/letter.dto';
+import { JwtService } from '@nestjs/jwt';
+import { LetterInfo } from '../../../../../shared/entities/letter_info.entity';
 
 describe('LetterGateway', () => {
   let app: INestApplication;
@@ -27,6 +32,7 @@ describe('LetterGateway', () => {
   let letterStateRepository: Repository<LetterState>;
   let userStateRepository: Repository<UserState>;
   let letterRepository: Repository<Letter>;
+  let jwtService: JwtService;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -34,16 +40,28 @@ describe('LetterGateway', () => {
         TypeOrmModule.forRoot({
           type: 'sqlite',
           database: ':memory:',
-          entities: [LetterState, UserState, Letter],
+          entities: [LetterState, UserState, Letter, LetterInfo],
           synchronize: true,
         }),
-        TypeOrmModule.forFeature([LetterState, UserState, Letter]),
+        TypeOrmModule.forFeature([LetterState, UserState, Letter, LetterInfo]),
       ],
-      providers: [LetterGateway, LetterLogicService, LetterDbService],
+      providers: [
+        LetterGateway,
+        LetterLogicService,
+        LetterDbService,
+        {
+          provide: JwtService,
+          useValue: {
+            verifyAsync: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     gateway = moduleFixture.get<LetterGateway>(LetterGateway);
+
+    jwtService = moduleFixture.get<JwtService>(JwtService);
 
     letterStateRepository = moduleFixture.get<Repository<LetterState>>(
       getRepositoryToken(LetterState),
@@ -67,14 +85,24 @@ describe('LetterGateway', () => {
   });
 
   beforeEach(async () => {
-    clientSocket = io('http://localhost:3000');
+    (jwtService.verifyAsync as jest.Mock).mockResolvedValue(userToken_1);
+    clientSocket = io('http://localhost:3000', {
+      auth: {
+        token: 'your-test-token',
+      },
+    });
     await new Promise<void>((res) => {
       clientSocket.on('connect', () => {
         res();
       });
     });
 
-    otherSocket = io('http://localhost:3000');
+    (jwtService.verifyAsync as jest.Mock).mockResolvedValue(userToken_2);
+    otherSocket = io('http://localhost:3000', {
+      auth: {
+        token: 'your-test-token',
+      },
+    });
     await new Promise<void>((res) => {
       otherSocket.on('connect', () => {
         res();
@@ -100,21 +128,29 @@ describe('LetterGateway', () => {
   describe('Method Test', () => {
     //* 같은 유저 아이디로 접속한 모든 클라이언트에게 메세지를 가는지
     it('sendMessageToClient Method Test', async () => {
-      const otherSocket_2 = io('http://localhost:3000');
+      (jwtService.verifyAsync as jest.Mock).mockResolvedValue(userToken_2);
+      const otherSocket_2 = io('http://localhost:3000', {
+        auth: {
+          token: 'your-test-token', // 모킹된 토큰을 설정
+        },
+      });
+
+      //* 접속완료 테스트
       await new Promise<void>((res) => {
         otherSocket_2.on('connect', () => {
           otherSocket_2.on(LETTER_MSG.INIT_MSG_RECEVIED, () => {
             res();
           });
-          otherSocket_2.emit(LETTER_MSG.INIT_MSG, 'test');
+          otherSocket_2.emit(LETTER_MSG.INIT_MSG);
         });
       });
 
+      // //* 접속완료 테스트
       await new Promise<void>((res) => {
         otherSocket.on(LETTER_MSG.INIT_MSG_RECEVIED, () => {
           res();
         });
-        otherSocket.emit(LETTER_MSG.INIT_MSG, 'test');
+        otherSocket.emit(LETTER_MSG.INIT_MSG);
       });
 
       const promise_1 = new Promise<void>((res) => {
@@ -133,7 +169,7 @@ describe('LetterGateway', () => {
         });
       });
 
-      gateway.sendMessageToClient('test', {
+      gateway.sendMessageToClient('user2', {
         id: 'sender',
         content: 'TEST',
       });
@@ -148,10 +184,11 @@ describe('LetterGateway', () => {
           res();
         });
 
+        //* 접속이롼료되면 readySenndMessageToClient를 통해 이 클라이언트에게 메시지를 보낸다
         otherSocket.on(LETTER_MSG.INIT_MSG_RECEVIED, () => {
           gateway.readySendMassageToClient(letterState);
         });
-        otherSocket.emit(LETTER_MSG.INIT_MSG, user_2.id);
+        otherSocket.emit(LETTER_MSG.INIT_MSG);
       });
     });
 
@@ -163,6 +200,7 @@ describe('LetterGateway', () => {
 
         gateway.saveTimeoutId('test', setTimeoutId);
         expect(gateway.userSetTimeoutKeyMap.get('test')).toBe(setTimeoutId);
+        clearTimeout(setTimeoutId);
       });
     });
   });
@@ -170,59 +208,62 @@ describe('LetterGateway', () => {
   describe('Subscribe Integration Test', () => {
     describe('Subscribe initial_data Test', () => {
       it('해당 유저가 편지가 존재하지 않을 때', (done) => {
-        const id = 'testUser';
-
         clientSocket.on(LETTER_MSG.INIT_MSG_RECEVIED, (data) => {
           expect(data).toBe('success');
-          expect(gateway.idKeyMap.has(id)).toBe(true);
+          expect(gateway.idKeyMap.has(userToken_1.username)).toBe(true);
           done();
         });
 
-        clientSocket.emit(LETTER_MSG.INIT_MSG, id);
+        clientSocket.emit(LETTER_MSG.INIT_MSG);
       });
 
       it('해당 유저가 편지가 존재할 때', (done) => {
-        const id = '2';
-
-        clientSocket.on(LETTER_MSG.SEND_MSG, (message: ReceiveLetterDTO) => {
+        otherSocket.on(LETTER_MSG.SEND_MSG, (message: ReceiveLetterDTO) => {
           expect(message.content).toBe('Test');
           expect(message.id).toBe('1');
           done();
         });
 
-        clientSocket.emit(LETTER_MSG.INIT_MSG, id);
+        otherSocket.emit(LETTER_MSG.INIT_MSG);
       });
     });
 
     describe('Subscribe espresso Test', () => {
       it('유저가 편지 보낸 후 다른 유저가 편지 받을 때', (done) => {
-        const id = '1';
-        const id_2 = '3';
+        (jwtService.verifyAsync as jest.Mock).mockResolvedValue(userToken_3);
+        const otherSocket_2 = io('http://localhost:3000', {
+          auth: {
+            token: 'your-test-token', // 모킹된 토큰을 설정
+          },
+        });
 
         clientSocket.on(LETTER_MSG.INIT_MSG_RECEVIED, () => {
           clientSocket.emit(LETTER_MSG.RECEIVE_MSG, { content: 'test' });
         });
 
-        otherSocket.on(LETTER_MSG.INIT_MSG_RECEVIED, () => {
-          clientSocket.emit(LETTER_MSG.INIT_MSG, id);
+        otherSocket_2.on(LETTER_MSG.INIT_MSG_RECEVIED, () => {
+          clientSocket.emit(LETTER_MSG.INIT_MSG);
         });
 
         clientSocket.on(
           LETTER_MSG.RECEIVE_MSG_RECEVIED,
           (data: LetterState) => {
             expect(data.letter_id).toBe(2);
-            expect(data.receiver_id).toBe('3');
-            expect(data.sender_id).toBe('1');
+            expect(data.receiver_id).toBe(userToken_3.username);
+            expect(data.sender_id).toBe(userToken_1.username);
           },
         );
 
-        otherSocket.on(LETTER_MSG.SEND_MSG, (data: ReceiveLetterDTO) => {
-          expect(data.id).toBe('1');
+        otherSocket_2.on(LETTER_MSG.SEND_MSG, (data: ReceiveLetterDTO) => {
+          expect(data.id).toBe(userToken_1.username);
           expect(data.content).toBe('test');
+          otherSocket_2.close();
           done();
         });
 
-        otherSocket.emit(LETTER_MSG.INIT_MSG, id_2);
+        otherSocket_2.on('connect', () => {
+          otherSocket_2.emit(LETTER_MSG.INIT_MSG);
+        });
       });
     });
   });
