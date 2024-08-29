d7a30d46327aa3933b2fd49f218e580800d30bd2
moonhr <moonhr153@gmail.com>
2024-08-29 12:27:23 +0900

    Fix | tailwind사용을 위한 config 수정

--- packages/client/next.config.mjs
+++ packages/client/next.config.mjs
@@ -1,7 +1,4 @@
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-// vanilla-extract 플러그인을 사용하는 Next.js 설정
-export default createVanillaExtractPlugin(nextConfig);
+export default nextConfig;
