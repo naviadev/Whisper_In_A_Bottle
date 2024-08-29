6f6a3c9fab6f83659d55ca6c444ff0369f3cff75
jaemin1005 <ghkdwoals1005@gmail.com>
2024-08-29 20:55:25 +0900

    Feat | letter.module.ts : 검증을 위해, JWT module 추가

--- packages/letter-service/src/letter/letter.module.ts
+++ packages/letter-service/src/letter/letter.module.ts
@@ -5,12 +5,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { LetterState } from '../../../../shared/entities/letter_state.entity';
 import { UserState } from '../../../../shared/entities/user_state.entity';
 import { Letter } from '../../../../shared/entities/letter.entity';
-import { typeOrmConfig } from '../../../../shared/config/env.config';
 import { LetterDbService } from './letter_db.service';
+import { JwtModule } from '@nestjs/jwt';
+import { typeOrmConfig } from '@shared/config/typeorm.config';
+import { configOptions } from '@shared/config/env.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { LetterInfo } from '@shared/entities/letter_info.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    TypeOrmModule.forFeature([LetterState, UserState, Letter]),
+    TypeOrmModule.forFeature([LetterState, UserState, Letter, LetterInfo]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule.forRoot(configOptions)],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET_KEY'), // 환경 변수에서 비밀 키 가져오기
+        signOptions: { expiresIn: '60m' }, // 토큰 유효기간
+      }),
+    }),
   ],
   providers: [LetterGateway, LetterDbService, LetterLogicService],
 })
