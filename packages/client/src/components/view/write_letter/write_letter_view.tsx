7cb2998b816b17c971bbc9993f414a718359aa87
moonhr <moonhr153@gmail.com>
2024-08-29 12:32:39 +0900

    Feat | 편지작성view
    전송시 소켓으로 이벤트를 발송하며
    화면이 비활성화된다.

---
+++ packages/client/src/components/view/write_letter/write_letter_view.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { useSocket } from "@client/src/app/context/socket_context";
+import { useView } from "@client/src/app/(organism)/view/context/view.context";
+
+/**
+ * * 작성된 편지를 보내기 위한 뷰 컴포넌트
+ * @returns
+ */
+export const WirteLetterView = () => {
+  const [message, setMessage] = useState("");
+
+  const socket = useSocket();
+
+  const { setSendLetter } = useView();
+
+  const handleSendMessage = () => {
+    if (socket && message.trim()) {
+      socket.emit("espresso", { content: message });
+      setMessage("");
+      setSendLetter(false);
+    }
+  };
+
+  const handleCloseWirteLetterView = () => {
+    setSendLetter(false);
+  };
+
+  return (
+    <div>
+      <textarea
+        placeholder="내용을 입력하세요"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button onClick={handleSendMessage}>보내기</button>
+      <button onClick={handleCloseWirteLetterView}>닫기</button>
+    </div>
+  );
+};
