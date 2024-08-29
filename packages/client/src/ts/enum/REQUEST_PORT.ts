70025e79271c138dc459fe5187fd40defa1b8447
moonhr <moonhr153@gmail.com>
2024-08-29 17:58:45 +0900

    WIB-48 Rename | REQUEST_POST 경로 설정

---
+++ packages/client/src/ts/enum/REQUEST_PORT.ts
@@ -0,0 +1,7 @@
+export enum REQUEST_PORT {
+  __SOCKET = "http://localhost:3002",
+  __LOGIN_PORT = "http://localhost:3001/auth/login",
+  __NEXT_SERVER_PORT = "http://localhost:3000",
+  __REGISTER_PORT = "http://localhost:3001/register",
+  __LETTER_SERVER_PORT = "http://localhost:3002",
+}
