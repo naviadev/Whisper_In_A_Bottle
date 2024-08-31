0dfdabf6407d7b85eb48714d14aabf15cff70867
moonhr <moonhr153@gmail.com>
2024-08-31 22:51:03 +0900

    WIB-51 Fix | 로그아웃 요청 시 쿠키를  확인하기 위한 설정 추가

--- packages/client/src/components/joystick/power_button/service/logout_axios.ts
+++ packages/client/src/components/joystick/power_button/service/logout_axios.ts
@@ -1,14 +1,16 @@
 //axios 로그아웃 요청
 import axios, { AxiosResponse } from "axios";
-import { Player } from "@client/src/ts/interface/player.interface";
 import { REQUEST_PORT } from "@client/src/ts/enum/REQUEST_PORT";
 
-export const logoutAxios = async (logoutData: Player): Promise<boolean> => {
+export const logoutAxios = async (): Promise<boolean> => {
   try {
     console.log("로그아웃 요청");
     const response: AxiosResponse = await axios.post(
       REQUEST_PORT.__LOGOUT_PORT,
-      logoutData
+      {},
+      {
+        withCredentials: true, // 쿠키를 함께 보내기 위해 설정
+      }
     );
     if (response.status === 200) {
       return true;
