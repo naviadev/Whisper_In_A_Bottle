7db08ba3d48c0387d42988f58299f742adfa4b2f
moonhr <moonhr153@gmail.com>
2024-08-31 22:46:42 +0900

    WIB-51 Feat | 전원버튼

---
+++ packages/client/src/components/joystick/power_button/power_button.tsx
@@ -0,0 +1,13 @@
+//전원버튼 = 로그아웃
+type PowerButtonPropsType = {
+  onClick: () => void;
+};
+
+//파워버튼 컴포넌트
+export const PowerButton: React.FC<PowerButtonPropsType> = ({ onClick }) => {
+  return (
+    <>
+      <div onClick={onClick} />
+    </>
+  );
+};
