22f2ce0aeb655cbd48c794d7821608d3d973e3b4
moonhr <moonhr153@gmail.com>
2024-08-31 22:14:21 +0900

    WIB-51 Fix | joysitck 프로바이더 연결

--- packages/client/src/app/(organism)/joystick/joystick.tsx
+++ packages/client/src/app/(organism)/joystick/joystick.tsx
@@ -1,11 +1,16 @@
 // src/client/joystick/joystick.tsx
 import React from "react";
+import { useJoystick } from "./context/joystick_context";
+
+import { PowerButton } from "@client/src/components/joystick/power_button/power_button";
 
 const Joystick: React.FC = () => {
+  const {showModal, setShowModal} = useJoystick();
   return (
     <div className="">
       <p>Joystick Component</p>
       {/* Joystick에 대한 추가 콘텐츠를 여기에 삽입할 수 있습니다 */}
+      <PowerButton />
     </div>
   );
 };
