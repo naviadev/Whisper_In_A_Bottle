9464b4acb5abee9d985984daa8a79be3668cc54e
moonhr <moonhr153@gmail.com>
2024-08-31 22:45:54 +0900

    WIB-51 Feat | 전원버튼 클릭 시 토큰 확인하여 모달창을 실행함

--- packages/client/src/app/(organism)/joystick/joystick.tsx
+++ packages/client/src/app/(organism)/joystick/joystick.tsx
@@ -2,15 +2,33 @@
 import React from "react";
 import { useJoystick } from "./context/joystick_context";
 
+import { useAll } from "@client/src/app/context/all_context";
+
 import { PowerButton } from "@client/src/components/joystick/power_button/power_button";
 
+import { LogoutModalContent } from "@client/src/components/joystick/logout_modal/logout_modal";
+
+import Modal from "@client/components/modal/modal";
+
 const Joystick: React.FC = () => {
-  const {showModal, setShowModal} = useJoystick();
+  const { showModal, setShowModal } = useJoystick();
+  const { getToken } = useAll();
+
+  const clickPowerButton = () => (getToken ? setShowModal(!showModal) : null);
+
+  const closeModal = () => {
+    setShowModal(!showModal);
+  };
   return (
     <div className="">
       <p>Joystick Component</p>
       {/* Joystick에 대한 추가 콘텐츠를 여기에 삽입할 수 있습니다 */}
-      <PowerButton />
+      <PowerButton onClick={clickPowerButton} />
+      {showModal ? (
+        <Modal isOpen={showModal} onClose={closeModal}>
+          <LogoutModalContent />
+        </Modal>
+      ) : null}
     </div>
   );
 };
