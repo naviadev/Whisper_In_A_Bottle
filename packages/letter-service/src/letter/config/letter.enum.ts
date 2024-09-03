0b1ca28e66323b083fcae50e309f3ff15cfca6b9
jaemin1005 <ghkdwoals1005@gmail.com>
2024-09-03 23:46:32 +0900

    New | letter에 대한 열거형 형식을 저장

---
+++ packages/letter-service/src/letter/config/letter.enum.ts
@@ -0,0 +1,8 @@
+export enum LETTER_CONFIG {
+  MAX_SAVE_COUNT = 10,
+}
+
+export enum LETTER_ERR {
+  SAVE_FAIL_LETTERSAVE_ERR_MSG = 'Failed Save LetterSave',
+  CHECK_FAIL_COUNT_LETTERSAVE_ERR_MSG = 'Failed checkMaxCount Method',
+}
