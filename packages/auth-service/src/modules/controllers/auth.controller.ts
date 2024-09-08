a5e0a9f430fadf6aa607c9621a59dd1a5b08ed37
naviadev <qo2oq1020@gmail.com>
2024-09-08 14:07:38 +0900

    Feat | auth.controller.ts : logout, refresh 엔드포인트 추가.
    1. 로그아웃 엔드포인트를 통해 쿠키 삭제, refresh token의 컬럼값을 null로
       변경

--- packages/auth-service/src/modules/controllers/auth.controller.ts
+++ packages/auth-service/src/modules/controllers/auth.controller.ts
@@ -1,13 +1,12 @@
-import { Controller, Post, Get, Body, Res, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus, Req } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { PlayerDTO } from '@shared/DTOs/player.dto';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 import { IpAddress } from '../../decorator/IpAddress.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
   /**
    * * 로그인 엔드포인트
    * 작성자 : @moonhr / 2024-07-23
@@ -22,7 +21,6 @@ export class AuthController {
     @Res() res: Response,
     @IpAddress() ip: string,
   ): Promise<VideoDecoderConfig> {
-    console.log('ㅇㅇ');
     try {
       const user = await this.authService.validateUser(data.id, data.password);
 
@@ -46,9 +44,34 @@ export class AuthController {
         .json({ success: false, message: 'Invalid credentials' });
     }
   }
-  /**
-   * * 로그아웃 엔드포인트
-   */
-  @Get('logout')
-  async logout() {} //TODO 로그아웃 시 처리 로직
+
+  @Post('refresh')
+  async refreshToken(@Req() req: Request, @Res() res: Response) {
+    const refreshToken = req.cookies['refresh_token']; // 클라이언트로부터 리프레시 토큰을 쿠키에서 가져옴
+
+    if (!refreshToken) {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ message: 'Refresh token not provided' });
+    }
+
+    try {
+      const newAccessToken =
+        await this.authService.refreshAccessToken(refreshToken);
+      res.status(HttpStatus.OK).json({ accessToken: newAccessToken });
+    } catch (error) {
+      res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ message: 'Invalid refresh token' });
+    }
+  }
+  @Post('logout')
+  async logout(@Req() req: Request, @Res() res: Response) {
+    const refreshToken = req.cookies.refresh_token;
+    if (refreshToken) {
+      await this.authService.invalidateRefreshToken(refreshToken);
+    }
+    res.clearCookie('refresh_token');
+    res.status(200).json({ message: 'Logged out successfully' });
+  }
 }
