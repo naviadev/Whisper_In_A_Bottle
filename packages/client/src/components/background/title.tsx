04c85b91b72abf41ea7296eabeb979d2f98a2e33
moonhr <moonhr153@gmail.com>
2024-09-08 20:34:49 +0900

    WIB-60 Style | title

---
+++ packages/client/src/components/background/title.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "../../styles/background/title.css";
+
+type titleProps = {
+  className?: string;
+};
+
+export const Title: React.FC<titleProps> = ({ className }) => {
+  return (
+    <div className={className}>
+      <div className="flex flex-row justify-between items-center h-full">
+        <div className="flex flex-col gap-2 ">
+          <div className="w-[43px] border border-[#f9e05f]" />
+          <div className="w-[43px] border border-[#f9e05f]" />
+        </div>
+        <div className="">
+          <h1 className="Whisper-In-A-Bottle1 relative">
+            Whisper In A Bottle
+            <h1 className="Whisper-In-A-Bottle2 absolute left-0 top-0">
+              Whisper In A Bottle
+            </h1>
+          </h1>
+        </div>
+        <div className="flex flex-col gap-2 ">
+          <div className="w-[43px] border border-[#f9e05f]" />
+          <div className="w-[43px] border border-[#f9e05f]" />
+        </div>
+      </div>
+    </div>
+  );
+};
