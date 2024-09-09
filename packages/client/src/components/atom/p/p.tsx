2d8758dc3ea758e5355bfefede13d2a8f4284290
naviadev <qo2oq1020@gmail.com>
2024-09-09 21:32:26 +0900

    Fix | p.tsx : P 기본 Style 삭제

--- packages/client/src/components/atom/p/p.tsx
+++ packages/client/src/components/atom/p/p.tsx
@@ -1,7 +1,7 @@
 import PProps from "./pProps";
 
 const P: React.FC<PProps> = ({ text, className }) => {
-  return <p className={`text-base leading-6 ${className}`}>{text}</p>;
+  return <p className={className}>{text}</p>;
 };
 
 export default P;
