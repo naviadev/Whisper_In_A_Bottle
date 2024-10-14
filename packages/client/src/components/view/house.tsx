636f17d0e7b716ce009c7772b353f91b68cc3354
moonhr <moonhr153@gmail.com>
2024-10-14 10:46:07 +0900

    WIB-61 Fix| 빈병 모델링 추가

--- packages/client/src/components/view/house.tsx
+++ packages/client/src/components/view/house.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import React, { useRef, useState, useEffect } from "react";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import * as THREE from "three";
+import { useSpring, animated } from "@react-spring/three";
+import {
+  Selection,
+  Select,
+  EffectComposer,
+  Outline,
+} from "@react-three/postprocessing";
 
 // GLTF 모델을 불러오는 컴포넌트
 function HouseModel(props: JSX.IntrinsicElements["group"]) {
@@ -17,12 +25,69 @@ function HouseModel(props: JSX.IntrinsicElements["group"]) {
   return <primitive object={scene} {...props} />;
 }
 
+const EmptyBottle = React.forwardRef(
+  (props: JSX.IntrinsicElements["group"] & { hover: boolean }, ref) => {
+    const { scene } = useGLTF("/models/empty_bottle/empty_bottle.gltf");
+    const { hover } = props;
+
+    useEffect(() => {
+      scene.traverse((child) => {
+        if (child instanceof THREE.Mesh) {
+          child.castShadow = true;
+          child.receiveShadow = true;
+        }
+      });
+    }, [scene]);
+
+    const { scale } = useSpring({
+      scale: hover ? 1 : 0.5,
+      config: { mass: 1, tension: 300, friction: 60 },
+    });
+
+    const AnimatedGroup = animated("group");
+
+    return (
+      <Select enabled={hover}>
+        <AnimatedGroup
+          ref={ref as React.RefObject<THREE.Group>}
+          {...props}
+          scale={scale}
+        >
+          <primitive object={scene} />
+        </AnimatedGroup>
+      </Select>
+    );
+  }
+);
+
+EmptyBottle.displayName = "EmptyBottle";
+
 function HouseScene() {
+  const [hoverBottle, setHoverBottle] = useState(false);
+  const bottleRef = useRef<THREE.Group>(null);
+
   return (
-    <>
+    <Selection>
+      <EffectComposer multisampling={8} autoClear={false}>
+        <Outline
+          blur
+          visibleEdgeColor={0xffffff}
+          hiddenEdgeColor={0x22090a}
+          edgeStrength={100}
+          width={1000}
+        />
+      </EffectComposer>
+      <EmptyBottle
+        ref={bottleRef}
+        hover={hoverBottle}
+        position={[1.2, 7.5, 0.1]}
+        onPointerOver={() => setHoverBottle(true)}
+        onPointerOut={() => setHoverBottle(false)}
+        onClick={() => console.log("Empty Bottle clicked")}
+      />
       <HouseModel />
       <OrbitControls />
-    </>
+    </Selection>
   );
 }
 
