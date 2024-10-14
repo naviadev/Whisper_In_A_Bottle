b144429587a4534e4b7e9d42d1c6ef3cc239c533
moonhr <moonhr153@gmail.com>
2024-10-14 10:45:53 +0900

    WIB-61 Fix | 소켓 이벤트시 활성화

--- packages/client/src/components/view/canvas.tsx
+++ packages/client/src/components/view/canvas.tsx
@@ -6,25 +6,80 @@ import { OceanScene } from "./ocean";
 import BottleScene from "./bottle";
 import HouseScene from "./house";
 import KeyboardControlledCamera from "./KeyboardControlledCamera";
-
+import { ReceivedLetter } from "./received_letter/received_letter";
+import { useSocket } from "@client/src/app/context/socket_context";
+import { useEffect, useState } from "react";
+import { useView } from "@client/src/app/(organism)/view/context/view_context";
 export const CanvasComponent = () => {
+  const socket = useSocket();
+  const {
+    onLetterView,
+    setOnLetterView,
+    receivedLetter,
+    setReceivedLetter,
+    receivedMessage,
+    setReceivedMessage,
+  } = useView();
+
+  useEffect(() => {
+    //초기 병 활성화
+    setOnLetterView(true);
+
+    if (socket) {
+      //사용자가 연결 되었음을 알리기 위한 socket 이벤트. 내용은 비어있음.
+      socket.emit("initial_data");
+
+      socket.on("latte", (message: { [key: string]: string }) => {
+        //편지 도착버튼을 활성화하기 위해 onLetterView를 true로 전환.
+        setOnLetterView(true);
+        setReceivedMessage(message.content);
+      });
+    }
+
+    return () => {
+      socket?.off("latte");
+      socket?.disconnect();
+    };
+  }, [
+    socket,
+    onLetterView,
+    receivedLetter,
+    setOnLetterView,
+    receivedMessage,
+    setReceivedMessage,
+  ]);
+
   return (
-    <Canvas style={{ height: "100%", backgroundColor: "white" }} shadows>
-      <ambientLight intensity={0.8} />
-      <directionalLight
-        intensity={1.2}
-        color="white"
-        position={[5, 18, 10]}
-        castShadow
-      />
-      <Suspense fallback={null}>
-        <OceanScene />
-        <HouseScene />
-        <BottleScene />
-        <KeyboardControlledCamera />
-      </Suspense>
-      <Sky sunPosition={[500, 150, -1000]} turbidity={0.1} />
-      <OrbitControls maxAzimuthAngle={Math.PI} maxPolarAngle={Math.PI} />
-    </Canvas>
+    <div className="relative h-full">
+      {receivedLetter && (
+        <div className="absolute inset-0 z-10">
+          <ReceivedLetter
+            letterMessage={receivedMessage!}
+            className="w-full h-full"
+          />
+        </div>
+      )}
+      <Canvas
+        style={{ height: "100%", backgroundColor: "white" }}
+        className="z-0 absolute"
+        shadows
+      >
+        <ambientLight intensity={0.8} />
+        <directionalLight
+          intensity={1.2}
+          color="white"
+          position={[5, 18, 10]}
+          castShadow
+        />
+        <Suspense fallback={null}>
+          <OceanScene />
+          <HouseScene />
+          {onLetterView ? <BottleScene /> : null}
+          <KeyboardControlledCamera />
+        </Suspense>
+        <Sky sunPosition={[500, 150, -1000]} turbidity={0.1} />
+        <OrbitControls maxAzimuthAngle={Math.PI} maxPolarAngle={Math.PI} />
+      </Canvas>
+    </div>
   );
 };
