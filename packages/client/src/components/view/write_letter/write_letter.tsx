181b331edbe6867016ecf2b4940730584c473afc
moonhr <moonhr153@gmail.com>
2024-10-15 11:38:59 +0900

    WIB-61 Fix | 편지 뷰에 종이 사운드 삽입

--- packages/client/src/components/view/write_letter/write_letter.tsx
+++ packages/client/src/components/view/write_letter/write_letter.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef } from "react";
 import Image from "next/image";
 import { WirteLetterView } from "./write_letter_view";
-import { useSocket } from "@client/src/app/context/socket_context";
+import PaperSound from "@client/src/components/sound/paper_sound";
 
 type WriteLetterPropType = {
   className?: string;
@@ -12,21 +12,23 @@ type WriteLetterPropType = {
  * @param 소켓으로 도착한 메세지
  * @returns
  */
-export const WriteLetter: React.FC<WriteLetterPropType> = ({
-  className,
-}) => {
+export const WriteLetter: React.FC<WriteLetterPropType> = ({ className }) => {
+  const paperSoundRef = useRef<HTMLAudioElement>(null);
   return (
-    <div className="top-0 left-0 z-10 flex items-center justify-center w-full h-full bg-[#D9D9D9]  bg-opacity-70 relative">
-      <div className="w-[60%] h-[60%] relative flex justify-center">
-        <WirteLetterView />
-        <Image
-          src="/ParchmentLetter.jpeg"
-          alt="편지 배경"
-          layout="fill"
-          objectFit="cover"
-          className="absolute z-0 rotate-90"
-        />
+    <>
+      <PaperSound ref={paperSoundRef} />
+      <div className="top-0 left-0 z-10 flex items-center justify-center w-full h-full bg-[#D9D9D9]  bg-opacity-70 relative">
+        <div className="w-[60%] h-[60%] relative flex justify-center">
+          <WirteLetterView />
+          <Image
+            src="/ParchmentLetter.jpeg"
+            alt="편지 배경"
+            layout="fill"
+            objectFit="cover"
+            className="absolute z-0 rotate-90"
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
