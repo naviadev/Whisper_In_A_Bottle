8715f216cfa0343a818dfc220512a975990ae815
moonhr <moonhr153@gmail.com>
2024-10-15 11:39:40 +0900

    WIB-61 Fix | 편지 작성부분에 병떨어지는 사운드 삽입

--- packages/client/src/components/view/write_letter/write_letter_view.tsx
+++ packages/client/src/components/view/write_letter/write_letter_view.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useSocket } from "@client/src/app/context/socket_context";
 import { useView } from "@client/src/app/(organism)/view/context/view_context";
+import WaterSplashSound from "@client/src/components/sound/water_splash_sound";
 
 /**
  * * 작성된 편지를 보내기 위한 뷰 컴포넌트
@@ -8,6 +9,7 @@ import { useView } from "@client/src/app/(organism)/view/context/view_context";
  */
 export const WirteLetterView = () => {
   const [message, setMessage] = useState("");
+  const waterSplashSoundRef = useRef<HTMLAudioElement>(null);
 
   const socket = useSocket();
 
@@ -16,7 +18,7 @@ export const WirteLetterView = () => {
   const handleSendMessage = () => {
     if (socket && message.trim()) {
       socket.emit("espresso", { content: message });
-      console.log("message", message);
+      waterSplashSoundRef.current?.play();
       setMessage("");
       setSendLetter(false);
     }
@@ -27,21 +29,24 @@ export const WirteLetterView = () => {
   };
 
   return (
-    <div className="absolute z-10 w-[60%] h-full">
-      <textarea
-        placeholder="write your letter"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        className="w-full h-full bg-transparent focus:outline-none placeholder:text-black"
-      />
-      <div className="flex justify-between w-full">
-        <button onClick={handleSendMessage} className="">
-          Send
-        </button>
-        <button onClick={handleCloseWirteLetterView} className="">
-          Cancel
-        </button>
+    <>
+      <WaterSplashSound ref={waterSplashSoundRef} />
+      <div className="absolute z-10 w-[60%] h-full">
+        <textarea
+          placeholder="write your letter"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="w-full h-full bg-transparent focus:outline-none placeholder:text-black"
+        />
+        <div className="flex justify-between w-full">
+          <button onClick={handleSendMessage} className="">
+            Send
+          </button>
+          <button onClick={handleCloseWirteLetterView} className="">
+            Cancel
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
