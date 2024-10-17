cf226088c24756e3028e634be7e63af7762ee5b8
naviadev <qo2oq1020@gmail.com>
2024-10-17 14:02:16 +0900

    Sytle |  write letter

--- packages/client/src/components/view/write_letter/write_letter_view.tsx
+++ packages/client/src/components/view/write_letter/write_letter_view.tsx
@@ -30,21 +30,23 @@ export const WirteLetterView = () => {
   };
 
   return (
-    <>
+<>
       <WaterSplashSound ref={waterSplashSoundRef} />
-      <textarea
-        placeholder="write your letter"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        className="w-full h-full bg-transparent focus:outline-none placeholder:text-black"
-      />
-      <div className="flex justify-between w-full">
-        <button onClick={handleSendMessage} className="">
-          Send
-        </button>
-        <button onClick={handleCloseWirteLetterView} className="">
-          Cancel
-        </button>
+      <div className="absolute z-10 w-[60%] h-full">
+        <textarea
+          placeholder="write your letter"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="w-full h-full bg-transparent focus:outline-none placeholder:text-black"
+        />
+        <div className="flex justify-between w-full">
+          <button onClick={handleSendMessage} className="">
+            Send
+          </button>
+          <button onClick={handleCloseWirteLetterView} className="">
+            Cancel
+          </button>
+        </div>
       </div>
     </>
   );
